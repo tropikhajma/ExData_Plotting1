d0fea602e3ca8228267e8aef714feb5f279e803a
Pavel Heimlich <tropikhajma@gmail.com>
2015-03-07 23:41:58 +0100

    fix plot1

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@ plot1 <- function() {
         source("R4P1.R")
         mydata<-getdata()
         png(file = "plot1.png", width = 480, height = 480, units = "px")
-        hist(mydata$'Global active power (kilowatts)', 
+        hist(mydata$Global_active_power, 
              col = 'red', 
              main = "Global Active Power", 
              xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
