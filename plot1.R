b0564a17dd91836185369b5a8f3f26716f8e5e35
Pavel Heimlich <tropikhajma@gmail.com>
2015-03-07 23:58:06 +0100

    fix plot1 transparency

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
 plot1 <- function() {
         source("R4P1.R")
         mydata<-getdata()
-        png(file = "plot1.png", width = 480, height = 480, units = "px")
+        png(file = "plot1.png", width = 480, height = 480, units = "px", bg = "transparent")
         hist(mydata$Global_active_power, 
              col = 'red', 
              main = "Global Active Power", 
